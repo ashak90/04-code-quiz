3e8a0de827a1fd496a814f8019b4047cb3da5604
Asha Kumar <ashakumar90@gmail.com>
2021-03-12 17:38:54 -0500

    added a div for the answer feeback, added the appropriate logic and styling

--- js/script.js
+++ js/script.js
@@ -4,26 +4,27 @@ const nextButton = document.getElementById("next-btn");
 const introSlide = document.getElementById("intro-slide");
 const questionPromptEl = document.getElementById("question-prompt-text");
 const answerButtonsDiv = document.getElementById("answer-buttons");
-const questionRight = document.getElementById("questions-right");
+const questionsRight = document.getElementById("questions-right");
 const totalQuestionsEl = document.getElementById("total-questions");
 const timeRemainderEl = document.getElementById("time-remainder");
 const gameOverModal = document.getElementById("game-over-mod-bg");
+const feedBackDiv = document.getElementById("answer-feedback");
 
 // Question Bank//
 const quizQuestions = [
     {
-        question: 'What is 2+2?',
-        answer: [
-            { text: '4', correct: true },
-            { text: '5', correct: false },
-            { text: '6', correct: false },
-            { text: '6', correct: false },
+        question: "What is 2+2?",
+        answers: [
+            { text: "4", correct: true },
+            { text: "5", correct: false },
+            { text: "6", correct: false },
+            { text: "6", correct: false },
 
         ]
     },
     {
         question: 'What is 5+5?',
-        answer: [
+        answers: [
             { text: '12', correct: false },
             { text: '10', correct: true },
             { text: '16', correct: false },
@@ -32,7 +33,40 @@ const quizQuestions = [
         ]
     }, {
         question: 'What is everything?',
-        answer: [
+        answers: [
+            { text: '4', correct: true },
+            { text: 'dddd5', correct: false },
+            { text: '6dcdcdc', correct: false },
+            { text: '6cddcd', correct: false },
+
+        ]
+    },
+
+    {
+        question: 'What is everything?',
+        answers: [
+            { text: '4', correct: true },
+            { text: 'dddd5', correct: false },
+            { text: '6dcdcdc', correct: false },
+            { text: '6cddcd', correct: false },
+
+        ]
+    },
+
+    {
+        question: 'What is everything?',
+        answers: [
+            { text: '4', correct: true },
+            { text: 'dddd5', correct: false },
+            { text: '6dcdcdc', correct: false },
+            { text: '6cddcd', correct: false },
+
+        ]
+    },
+
+    {
+        question: 'What is everything?',
+        answers: [
             { text: '4', correct: true },
             { text: 'dddd5', correct: false },
             { text: '6dcdcdc', correct: false },
@@ -46,7 +80,7 @@ const quizQuestions = [
 var currentQuestionIndex = 0;
 var totalCorrect = 0;
 var questionsAttempted = 0;
-const gameDuration = 5;
+const gameDuration = 25;
 var gameDurationInSec = gameDurationInSec;
 var gameDurationInMin = gameDurationInSec / 60;
 var totalQuestions = quizQuestions.length;
@@ -56,16 +90,15 @@ var myInterval;
 //Event Listeners//
 
 startButton.addEventListener("click", startGame);
-// nextButton.addEventListener("click", () => {
-//     currentQuestionIndex++;
-//     nextQuestionSlide();
-//   });
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++;
+    nextQuestionSlide();
+});
 
 // Start Game
 
 function startGame() {
-    console.log('Started')
-    currentQuestionsIndex = 0
+    currentQuestionIndex = 0
     shuffleQuestions = quizQuestions.sort(() => Math.random() - .5)
     totalCorrect = 0
     questionsAttempted = 0
@@ -94,7 +127,7 @@ function startTimer() {
             secondHand = "0" + secondHand;
         }
 
-        timer.innerHTML = "Time: "+`${minuteHand}:${secondHand}`;
+        timer.innerHTML = "Time: " + `${minuteHand}:${secondHand}`;
 
         if (gameDurationInSec <= 0) {
             gameOver();
@@ -102,7 +135,7 @@ function startTimer() {
     }, 1000);
 }
 
-function scoreDisplay (){
+function scoreDisplay() {
     var score = document.createElement("div");
     score.setAttribute("id", "score-readout");
     document.body.appendChild(score);
@@ -116,196 +149,90 @@ function nextQuestionSlide() {
 
 function resetAnswers() {
     while (answerButtonsDiv.firstChild) {
-      answerButtonsDiv.removeChild(answerButtonsDiv.firstChild);
+        answerButtonsDiv.removeChild(answerButtonsDiv.firstChild);
     }
     nextButton.classList.add("hide");
+    feedBackDiv.classList.add ("hide")
 }
 
 function showNextQuestion(question) {
-    questionPromptEl.innerText=quizQuestions[currentQuestionIndex].question;
-    quizQuestions[currentQuestionIndex].answer.forEach((answer) => {
+    questionPromptEl.innerText = quizQuestions[currentQuestionIndex].question;
+    quizQuestions[currentQuestionIndex].answers.forEach((answer) => {
         const button = document.createElement("button");
         button.innerText = answer.text;
         button.setAttribute("class", "btn btn-outline-info");
         if (answer.correct) {
-          button.dataset.correct = answer.correct;
+            button.dataset.correct = answer.correct;
         }
         button.addEventListener("click", selectAnswer);
         answerButtonsDiv.appendChild(button);
     });
 }
 
-function selectAnswer(event){
+function selectAnswer(event) {
     questionsAttempted++;
     var selectedButton = event.target;
-    Array.from(answerButtonsDiv.children).forEach((button) => {
-        setStatusClasss(button, button.dataset.correct);
-    });
-    if (selectedButton.dataset.correct){
+    feedBackDiv.classList.remove("hide")
+    // var createDiv = document.createElement("div");
+    // createDiv.setAttribute("id", "createDiv");
+    // document.body.appendChild(createDiv);
+
+    // Array.from(answerButtonsDiv.children).forEach((button) => {
+    //     setStatusClass(button);
+    // });
+    if (selectedButton.dataset.correct) {
         totalCorrect++;
+        feedBackDiv.innerHTML = "Correct! The answer is:  ";
+
     } else if (
         selectedButton.dataset.correct === false &&
         gameDurationInSec < 10
+        
     ) {
         gameDurationInSec = 0;
+        feedBackDiv.innerHTML = "Incorrect! The answer is:  ";
+
     } else {
         gameDurationInSec -= 10;
-    }
-}
+        feedBackDiv.innerHTML = "Incorrect! The answer is:  ";
 
-function setStatusClass(element, correct) {
-    clearStatusClass(element);
-    if (correct) {
-      element.classList.add("btn-success");
-      element.classList.remove("btn-outline-info");
+
+    }
+
+
+    // function setStatusClass(element, correct) {
+    //     clearStatusClass(element);
+    //     if (correct) {
+    //         element.classList.add("btn-success");
+    //         element.classList.remove("btn-outline-info");
+    //     } else {
+    //         element.classList.add("btn-danger");
+    //         element.classList.remove("btn-outline-info");
+    //     }
+    // }
+
+    // function clearStatusClass(element) {
+    //     element.classList.remove("btn-success");
+    //     element.classList.remove("btn-danger");
+    // }
+
+    if (questionsAttempted < totalQuestions) {
+        nextButton.classList.remove("hide");
     } else {
-      element.classList.add("btn-danger");
-      element.classList.remove("btn-outline-info");
+        gameOver();
     }
 }
 
-// if (questionsAttempted < totalQuestions) {
-//     nextButton.classList.remove("hide");
-//   } else {
-//     gameOver();
-//   }
-  function gameOver() {
-    nextButton.classList.add("hide");
+function gameOver() {
+    // nextButton.classList.add("hide");
     clearInterval(myInterval);
-    // document.getElementById("timer-readout").classList.add("hide");
     gameOverModal.classList.add("modal-active");
     startButton.innerText = "Play Again";
     startButton.classList.remove("hide");
     questionsRight.innerHTML = totalCorrect;
     totalQuestionsEl.innerHTML = totalQuestions;
-  
+
     if (gameDurationInSec >= 0) {
-      timeRemainderEl.innerHTML = gameDurationInSec;
+        timeRemainderEl.innerHTML = gameDurationInSec;
     } else timeRemainderEl.innerHTML = "0";
-  }
-
-
-// ==============My original javacript code=============
-// const startButton = document.getElementById('start')
-// const questionContainerElement = document.getElementById('question-container')
-// const questionElement = document.getElementById('question')
-// const answerButtonsElement = document.getElementById('answer-buttons')
-// const finishElement = document.getElementById('finish')
-// var timerEl = document.getElementById('timer')
-
-// let currentQuestions = [];
-// let acceptingAnswers = false;
-// let score = 0;
-// let availableQuestions = [];
-// let shuffleQuestions, currentQuestionsIndex
-
-// startButton.addEventListener('click', startGame)
-
-// function startGame() {
-//     console.log('Started')
-//     startButton.classList.add('hide')
-//     shuffleQuestions = questions.sort(() => Math.random() - .5)
-//     currentQuestionsIndex = 0
-//     questionContainerElement.classList.remove('hide')
-//     setTime()
-//     setNextQuestion()
-// }
-
-// function endGame (){
-//     questionContainerElement.classList.add('hide')
-
-//     finishElement.classList.remove ('hide')
-
-// }
-
-// function setTime () {
-//     var timeLeft = 5;
-
-//     // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-//     var timeInterval = setInterval(function () {
-//         // As long as the `timeLeft` is greater than 1
-//         if (timeLeft > 1) {
-//             // Set the `textContent` of `timerEl` to show the remaining seconds
-//             timerEl.textContent = 'Timer: ' + timeLeft + ' seconds remaining';
-//             // Decrement `timeLeft` by 1
-//             timeLeft--;
-//         } else if (timeLeft === 1) {
-//             // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-//             timerEl.textContent = 'Timer: ' + timeLeft + ' second remaining';
-//             timeLeft--;
-//         }
-//         else if (timeLeft===0) {
-//             // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-//             timerEl.textContent = timeLeft + ' seconds remaining';
-//             // Use `clearInterval()` to stop the timer
-//             clearInterval(timeInterval);
-//             // Call the `displayMessage()` function
-//             endGame();
-//           }
-//     }, 1000);
-// }
-// function setNextQuestion() {
-//     resetState()
-//     showQuestion(shuffleQuestions[currentQuestionsIndex])
-
-// }
-// function showQuestion(question) {
-//     questionElement.innerText = question.question
-//     question.answer.forEach(answer => {
-//         const button = document.createElement('button')
-//         button.innerText = answer.text
-//         button.classList.add('btn')
-//         if (answer.correct) {
-//             button.dataset.correct = answer.correct
-//         }
-//         // else {
-//         //     return 'Wrong!'
-//         // }
-//         button.addEventListener('click', selectAnswer)
-//         answerButtonsElement.appendChild(button)
-//     })
-// }
-
-// function resetState() {
-//     while (answerButtonsElement.firstChild) {
-//         answerButtonsElement.removeChild
-//         (answerButtonsElement.firstChild)
-//     }
-// }
-// function selectAnswer(e) {
-//     const selectButton = e.target
-//     const correct = selectButton.dataset.correct
-
-// }
-
-// const questions = [
-//     {
-//         question: 'What is 2+2?',
-//         answer: [
-//             { text: '4', correct: true },
-//             { text: '5', correct: true },
-//             { text: '6', correct: true },
-//             { text: '6', correct: true },
-
-//         ]
-//     },
-//     {
-//         question: 'What is 67676?',
-//         answer: [
-//             { text: 'ndd', correct: true },
-//             { text: 'dndndjd', correct: true },
-//             { text: 'dmdndjd', correct: true },
-//             { text: '6', correct: true },
-
-//         ]
-//     }, {
-//         question: 'What is everything?',
-//         answer: [
-//             { text: '4', correct: true },
-//             { text: 'dddd5', correct: true },
-//             { text: '6dcdcdc', correct: true },
-//             { text: '6cddcd', correct: true },
-
-//         ]
-//     },
-// ]
+}
